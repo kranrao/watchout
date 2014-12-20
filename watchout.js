0c5e54c2d4922b0d0ccb1c594a780cfef9f27740
kranrao <k.ranrao@gmail.com>
2014-12-19 16:20:57 -0800

    jQueryfied asteroid movement

--- watchout.js
+++ watchout.js
@@ -1,4 +1,4 @@
-// Object decorator function that generate a random position of an asteroid within the canvas
+//Function to generate a random position of an asteroid within the canvas
 var positionGenerator = function (asteroids) {
   for (var i = 0; i < asteroids.length; i++) {
     var top = Math.floor(Math.random() * height);
@@ -10,7 +10,7 @@ var positionGenerator = function (asteroids) {
 };
 
 // Creates asteroids object data
-var asteroids = [{},{},{},{},{}];
+var asteroids = [{"a": "One"},{"b": "Two"},{"c": "Three"},{"d": "Four"},{"e": "Four"}];
 
 // Creates SVG canvas in html
 var width = 960,
@@ -21,26 +21,39 @@ var space = d3.select("body").append("div")
     .attr("height", height + "px")
     .attr("class", "space");
 
-// Create asteroid nodes
-var asteroid = space.selectAll("div").data(asteroids);
+// Initializes space
+var initGame = function (asteroids) {
+  // Create asteroid nodes
+  var asteroid = space.selectAll("div").data(asteroids);
 
-// Initial placement of asteroid nodes on canvas
-asteroid.enter().append("img")
-    .attr("class", "asteroid")
-    .style("top", function (d) { return d['top'] + "px";})
-    .style("left", function (d) { return d['left'] + "px";});
+  // Gets positions for asteroid nodes
+  positionGenerator(asteroids);
 
-// Runs initial placement
-positionGenerator(asteroids);
+  // Places asteroid nodes on canvas
+  asteroid.enter().append("img")
+      .attr("class", "asteroid")
+      .style("top", function (d) { return d['top'] + "px";})
+      .style("left", function (d) { return d['left'] + "px";});
 
-// Interval
+};
 
+initGame(asteroids);
 
-  // Select asteroids
+// Move asteroids after initialize
 
-  // Update positioning
+var moveAsteroids = function () {
+  var asteroid = d3.selectAll("img")
+      .style("top", function (d) {
+        return Math.floor(Math.random() * height) + "px";
+      })
+      .style("left", function (d) {
+        return Math.floor(Math.random() * width) + "px";
+      })
 
+}
 
+// Interval to change asteroid node positions
 
-
-//positionGenerator();
+setInterval(function () {
+  moveAsteroids();
+}, 200);
